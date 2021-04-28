24e150353e8b55503a7f08182536095db18b878a
Traiq <mr.tm84@gmail.com>
2021-04-28 22:43:00 +0200

    Rest API

--- index.js
+++ index.js
@@ -1,7 +1,92 @@
-const { app } = require('./core'); 
+const { app } = require("./core");
+const { db, update } = require("./db");
+const Joi = require("joi");
+
 
 app.listen(3000, () => {
-    console.log('API for smart home 1.1 up n running.')
-})
+  console.log("API for smart home 1.1 up n running.");
+});
 
 /* CODE YOUR API HERE */
+
+const controlDevice = (id) => {
+
+  const device = db.get("devices").find({ id: id }).value();
+  
+  return device;
+
+};
+
+
+
+app.get("/devices", (req, res) => {
+  res.send(db);
+});
+
+
+
+app.get("/devices/:type/:id/switch", (req, res) => {
+
+    let updatedDevice = controlDevice(req.params.id);
+
+    if (req.params.type !== "Lock" && req.params.type === updatedDevice.type) {
+
+            updatedDevice.on = !updatedDevice.on;
+
+            if (updatedDevice.on === true) {
+
+            res.send(` ${updatedDevice.type} is on`);
+
+            } else {
+
+            res.send(` ${updatedDevice.type} is off`);
+
+            }
+
+    } else {
+
+            updatedDevice.locked = !updatedDevice.locked;
+
+            if (updatedDevice.locked === true) {
+
+            res.send(` ${updatedDevice.type} is on`);
+
+            } else {
+
+            res.send(` ${updatedDevice.type} is off`);
+
+            } 
+        }
+});
+
+app.put('/devices/:id', (req, res) => {
+    let updatedDevice = controlDevice(req.params.id);
+
+    if (!updatedDevice) res.status(400).send('the device not found')
+
+    const { error } = validateInput(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    
+    
+    updatedDevice.on = req.body.on
+    updatedDevice.brightness = req.body.brightness
+    updatedDevice.color = req.body.color
+    updatedDevice.locked = req.body.locked
+
+
+
+
+    res.send(updatedDevice)
+
+})
+
+function validateInput(device) {
+  // "npm i joi" install joi validation
+  const schema = Joi.object({
+    on: Joi.boolean(),
+    brightness: Joi.number(),
+    color: Joi.string().min(4),
+    locked: Joi.boolean()
+  });
+  return schema.validate(device);
+}
